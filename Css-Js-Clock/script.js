a882077101f3472ea9575d1c7bfeecd1322745b1
sujan2000 <meena.sujan14@gmail.com>
2025-11-26 22:18:36 +0530

    css done

---
+++ Css-Js-Clock/script.js
@@ -0,0 +1,22 @@
+const hourHand = document.querySelector(".hour-hand");
+const minHand = document.querySelector(".min-hand");
+const secHand = document.querySelector(".second-hand");
+
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+    const minutes = now.getMinutes();
+    const minDegrees = ((minutes/60)*360)+90;
+    minHand.style.transform = `rotate(${minDegrees}deg)`
+
+    
+    const hours = now.getHours();
+    const hoursDegrees = ((hours/12)*360)+90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+
+}
+setInterval(setDate, 1000)
